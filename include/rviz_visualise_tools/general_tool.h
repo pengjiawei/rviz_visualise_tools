d9995f37c920be868510f48cce5608ef1f83ca70
pengjiawei <917527280@qq.com>
2018-03-09 14:22:43 +0800

    transfer data in socket with boost serialized

--- include/rviz_visualise_tools/general_tool.h
+++ include/rviz_visualise_tools/general_tool.h
@@ -8,7 +8,118 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <fstream>
+#include <netinet/in.h>
+#include<arpa/inet.h>
+#include <unistd.h>
+#include <tiff.h>
+#include <boost/thread.hpp>
+#include <boost/thread/condition.hpp>
+#include <boost/thread/mutex.hpp>
 using namespace std;
+const int BUFF_SIZE = 1024;
+bool work_done;
+boost::mutex mutex;
+
+boost::condition cv;
+void createSocketAndReceive(char* recvData,int port,std::string recvFileName) {
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    bzero(&(server_addr.sin_zero), 8);
+
+    int server_sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock_fd == -1) {
+        printf("Create goal socket failure!");
+    }
+    //绑定socket
+    int bind_result = bind(server_sock_fd, (struct sockaddr *) &server_addr,
+                           sizeof(server_addr));
+    if (bind_result == -1) {
+        printf("Set goal port failure!");
+    }
+    //listen
+    printf("i am listening!!!!!!!!!!\n");
+    if (listen(server_sock_fd, 3) == -1) {
+        printf("Listen goal port error!");
+    }
+
+    fd_set server_fd_set;
+    int max_fd = -1;
+//    struct timeval tv;
+//    int times = 0;
+//    tv.tv_sec = 5;
+//    tv.tv_usec = 0;
+
+
+    while (true) {
+
+        FILE* file = fopen(recvFileName.c_str(),"a+");
+        FD_ZERO(&server_fd_set);
+        FD_SET(server_sock_fd, &server_fd_set);
+
+        int ret = select(server_sock_fd + 1, &server_fd_set, NULL, NULL, NULL);
+        printf("select return = %d\n", ret);
+        if (ret > 0) {
+            int client_sock_fd = 0;
+            bool flag = FD_ISSET(server_sock_fd, &server_fd_set);
+            printf("is in the set? = %d\n", flag);
+            if (flag) {
+                struct sockaddr_in client_address;
+                memset(&client_address, 0, sizeof(client_address));
+
+                socklen_t address_len = sizeof(client_address);
+                printf("i am accept!!!!!!!!!!\n");
+                client_sock_fd = accept(server_sock_fd,
+                                        (struct sockaddr *) &client_address, &address_len);
+
+                printf("client_sock_fd = %d\n", client_sock_fd);
+                if (client_sock_fd <= 0) {
+                    printf("client_sock_tf_in_controller < 0");
+                }
+            }
+
+            if (client_sock_fd != 0) {
+
+
+                memset(recvData, 0, BUFF_SIZE);
+
+                printf("sizeof recv Data = %d\n",sizeof(recvData));
+                size_t byte_num = 0;
+                size_t all = 0;
+                int offset = 0;
+
+                while(byte_num = recv(client_sock_fd, recvData, BUFF_SIZE,
+                                      0)){
+                    printf("recv_return = %d\n", byte_num);
+                    all += byte_num;
+
+                    int write_length = fwrite(recvData, sizeof(char),byte_num,file);
+                    printf("write_length = %d\n",write_length);
+                    bzero(recvData,BUFF_SIZE);
+                }
+                printf("all recv size = %d\n",all);
+                fclose(file);
+                boost::mutex::scoped_lock lock(mutex);
+                cv.notify_one();
+                lock.unlock();
+                if (byte_num > 0) {
+                } else if (byte_num < 0) {
+                    close(client_sock_fd);
+                    client_sock_fd = 0;
+                } else {
+                    FD_CLR(client_sock_fd, &server_fd_set);
+                    client_sock_fd = 0;
+                }
+            }
+        } else if (ret == 0) {
+            FD_ZERO(&server_fd_set);
+            FD_SET(server_sock_fd, &server_fd_set);
+        }
+    }
+}
 void readPgm(std::string pgm_file_path,unsigned int& width ,unsigned int& height,std::vector<int>& value_vec){
     int row = 0, col = 0;
     ifstream infile(pgm_file_path);
@@ -59,4 +170,6 @@ void readPgm(std::string pgm_file_path,unsigned int& width ,unsigned int& height
     fclose(file);
     infile.close();
 }
+
+
 #endif //RVIZ_VISUALISE_TOOLS_GENERAL_TOOL_H
--- src/demo_main.cpp
+++ src/demo_main.cpp
@@ -3,6 +3,8 @@
 #include <sstream>
 #include <rviz_visualise_tools/visual_tool.h>
 #include <rviz_visualise_tools/general_tool.h>
+#include <boost/archive/text_iarchive.hpp>
+
 void readCostMapFromFile(std::vector<int>& value_vec);
 int main(int argc,char** argv) {
     std::cout << "Hello, World!" << std::endl;
@@ -11,27 +13,35 @@ int main(int argc,char** argv) {
     visual_tool tool("path","map");
 
     std::vector<double> x_vec,y_vec;
-    double index = 0.0;
     ros::Duration d(5);
     ros::Rate r(d);
-    for (int i = 0; i < 480; ++i) {
-        x_vec.push_back(index);
-        y_vec.push_back(index);
-        index += 1.0;
-    }
     std::vector<int> value_vec;
     unsigned int width,height;//willow-full.pgm,tb_condo_2.pgm
-    readPgm("/home/pengjiawei/willow-full.pgm",width,height,value_vec);
-//    readCostMapFromFile(value_vec);
-//    tool.publishOccupancyGrid(1,480,480,value_vec);
-    printf("value_vec size = %d\n",value_vec.size());
-    printf("width = %d, height =%d\n",width,height);
+//    readPgm("/home/pengjiawei/map.pgm",width,height,value_vec);
+    work_done = false;
+    char* recvData;
+    recvData = new char[BUFF_SIZE];
+    boost::thread recvThread(createSocketAndReceive,recvData,12345,"/tmp/recvFile.txt");
+
 //    tool.publishPgm(0.05,2,2,value_vec);
-    while(ros::ok()){
-        tool.publishPath(x_vec,y_vec);
-//        tool.publishOccupancyGrid(1,480,480,value_vec);
-        tool.publishPgm(0.01,height,width,value_vec);
+    while(ros::ok() && !work_done){
+        boost::mutex::scoped_lock lock(mutex);
+        cv.wait(mutex);
+        lock.unlock();
+
+        std::ifstream file("/tmp/recvFile.txt");
+        boost::archive::text_iarchive ia(file);
+        int i;
+        for(int32 y = 0 ; y < 480 ; ++y){
+            for(int32 x = 0; x< 480 ; ++x){
+                ia >> i;
+                value_vec.push_back(i);
+            }
+        }
+        tool.publishOccupancyGrid(0.01,480,480,value_vec);
+//        tool.publishPgm(0.01,height,width,value_vec);
         ros::spinOnce();
+
         ROS_INFO("publish");
         r.sleep();
     }
